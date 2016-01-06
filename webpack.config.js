7f0664e8238250b6b58f45c61ad9fe6c29a56140
Neven Đuranec <neven@burza.hr>
2016-01-06 18:09:30 +0100

    Initial

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
 var rootDir = __dirname;
 var srcDir = rootDir + "/src";
@@ -8,47 +8,21 @@ var distDir = rootDir + "/dist";
 module.exports = {
     context: rootDir, // a base directory to resolve the “entry”
 
-    entry: {
-        entry1: srcDir + '/entry1.js',
-        entry2: srcDir + '/entry2.js'
-    },
+    entry: 'main.js',
 
     output: {
         path: distDir,
-        filename: "[name].js" // [name] means we are going to use the "key" value of each entry as the bundle file name
+        publicPath: '/js/',
+        filename: "main-built.js" // [name] means we are going to use the "key" value of each entry as the bundle file name
     },
 
     resolve: {
-        extensions: ['', '.js', '.jsx'] // resolve file extentions so that we don't have to specify the extention for js and jsx files
+        extensions: ['', '.js', '.jsx'], // resolve file extentions so that we don't have to specify the extention for js and jsx files,
+        modulesDirectories: ['js', 'node_modules']
     },
 
-    // loaders for different types of resources. For jsx and es6, we uses babel loader.
-    module: {
-        loaders: [
-            { test: /\.html$/, exclude: /tmp/, loader: "ng-cache-loader" },
-            { test: /\.jsx?$/, exclude: /(node_modules|tmp)/, loader: 'babel-loader' },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.(png|jpg|woff|woff2|eot|ttf|otf)/, loader: 'url-loader' },
-            { test: /\.svg/, loader: 'file?name=/img/[hash].[ext]?' },
-            { test: /\.less/, loader: "style-loader!css-loader!less-loader" }
-        ]
-    },
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "commons",
-            filename: "commons.js",
-            chunks: ["entry1", "entry2"]
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Boxes',
-            filename: '/entry1.html', // relative path from "output" directory
-            template: srcDir + '/entry1.html' // source file
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Circles',
-            filename: '/entry2.html', // relative path from "output" directory
-            template: srcDir + '/entry2.html' // source file
-        })
+        commonsPlugin
     ]
 };
